5e94c08ad54b3a7a01268e3e9bc059c59e233c5b
Ryan Lau <ryanlau92@gmail.com>
2016-02-24 15:38:14 -0600

    Completed exercise 2

--- src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise2/Accumulator.java
@@ -1,4 +1,13 @@
 package com.thoughtworks.tw101.exercises.exercise2;
 
 public class Accumulator {
+    int total = 0;
+
+    public void increment(){
+        total += 1;
+    }
+
+    public void total(){
+        System.out.println(total);
+    }
 }
