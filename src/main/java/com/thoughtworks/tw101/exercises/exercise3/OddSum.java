3be55866b0a1c802cc01a68245bdfce8f5552884
Ryan Lau <ryanlau92@gmail.com>
2016-02-24 16:25:06 -0600

    Added exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -2,7 +2,14 @@ package com.thoughtworks.tw101.exercises.exercise3;
 
 public class OddSum {
 
+
     public int of(int start, int end) {
-        return -1;
+        int totalOddSum = 0;
+        for (int i = start; i < end; i++){
+            if(i % 2 != 0){
+                totalOddSum += i;
+            }
+        }
+        return totalOddSum;
     }
 }
